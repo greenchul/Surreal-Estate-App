8ee0f1a90ce330c687b60bd346524ced1405269b
Rachel Sanham <green_chul@hotmail.com>
2022-02-09 19:28:18 +0000

    Project set up

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Surreal Estate</h1>
+    </div>
+  );
+};
+
+export default App;
