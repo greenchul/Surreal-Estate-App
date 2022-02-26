ef3713e01a7ee947e5fc6270b7fcb109e6e4b479
Rachel Sanham <green_chul@hotmail.com>
2022-02-26 15:18:57 +0000

    Added facebook login

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "../styles/App.css";
 import NavBar from "./NavBar";
@@ -6,9 +6,21 @@ import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [userID, setUserID] = useState("");
+
+  const handleLogin = (response) => {
+    console.log(response);
+    setUserID(response.userID);
+  };
+
+  const handleLogout = () => {
+    window.FB.logout();
+    setUserID("");
+  };
   return (
     <div className="App">
-      <NavBar />
+      <NavBar onLogin={handleLogin} userID={userID} onLogout={handleLogout} />
       <Switch>
         <Route exact path="/" component={Properties} />
         <Route exact path="/add-property" component={AddProperty} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import propTypes from "prop-types";
+// eslint-disable-next-line no-unused-vars
+import ReactDOM from "react-dom";
+import FacebookLogin from "react-facebook-login";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
 
-const NavBar = () => {
+const NavBar = ({ onLogin, userID, onLogout }) => {
   return (
     <div className="navbar">
       <ul>
@@ -19,9 +23,28 @@ const NavBar = () => {
             Add a property
           </Link>
         </li>
+        <li>
+          {!userID ? (
+            <FacebookLogin
+              callback={onLogin}
+              appId="919435078722829"
+              fields="name,email"
+            />
+          ) : (
+            <button type="button" onClick={onLogout}>
+              Sign out
+            </button>
+          )}
+        </li>
       </ul>
     </div>
   );
 };
 
 export default NavBar;
+
+NavBar.propTypes = {
+  onLogin: propTypes.func.isRequired,
+  userID: propTypes.string.isRequired,
+  onLogout: propTypes.func.isRequired,
+};
